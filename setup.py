7c13f81bf2ff1956d548d85705966e546ca870bd
Ronald Sprouse <ronald@berkeley.edu>
2023-03-01 15:27:17 -0800

    Switch to importlib_resources

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from distutils.core import setup
 
 setup(
   name = 'phonlab',
-  version = '0.9.5',
+  version = '0.9.6',
   packages = ['phonlab', 'phonlab.third_party'],
+  install_requires=[
+    'importlib_resources; python_version < "3.9"',
+    'numpy',
+    'pandas',
+    'scipy',
+  ],
   scripts = [
   ],
-  package_data = {'phonlab': ['data/phonemap.txt']},
+  include_package_data=True,
   classifiers = [
     'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering'
